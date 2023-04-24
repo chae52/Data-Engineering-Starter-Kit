dc10e541228a86540d4ce604e6f9a96e0726f204
chae52 <fjdksla98@naver.com>
2023-04-24 10:25:29 +0900

    [FIX] 틀린 부분

--- Week2/2-4_RevenueByChannelAndMonth.sql
+++ Week2/2-4_RevenueByChannelAndMonth.sql
@@ -1,5 +1,3 @@
-%%sql
-
 SELECT LEFT(ts,7) as ym,channel,
 COUNT(DISTINCT userid) AS uniqueUsers,
 COUNT(DISTINCT CASE WHEN amount>0 THEN userid END) AS paidUsers,
@@ -7,7 +5,7 @@ COUNT(DISTINCT CASE WHEN amount>0 THEN userid END) AS paidUsers,
 SUM(str.amount) as grossRevenue,
 SUM(CASE WHEN str.refunded is FALSE THEN amount END) AS netGrossRevenue
 FROM raw_data.user_session_channel usc 
-JOIN raw_data.session_transaction str ON usc.sessionid = str.sessionid
+LEFT JOIN raw_data.session_transaction str ON usc.sessionid = str.sessionid
 JOIN raw_data.session_timestamp st ON usc.sessionid = st.sessionid
 GROUP BY CHANNEL, ym
 ORDER BY YM, channel;
